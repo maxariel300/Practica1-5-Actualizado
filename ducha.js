3786c7af9967b839b14b9edc4ddaac3391a128f7
Ariel Alvarez <arialvarez2019@gmail.com>
2023-06-05 11:28:48 -0300

    Actualizado

---
+++ ducha.js
@@ -0,0 +1,25 @@
+document.querySelector("body").innerHTML = `
+<h1>Instructivo para bañarse</h1>
+
+<ol>
+  <li>Entrar al baño</li>
+  <li>Quitarse la vestimenta</li> 
+  <li>Abrir la canilla</li>
+  <li>Regular la temperatura del agua</li> 
+  <li>Meterse dentro de la ducha</li>
+  <li>Agarrar el shampoo</li> 
+  <li>Aplicar sobre el pelo</li> 
+  <li>Enjabonarse y restregar</li> 
+  <li>Enjugarse</li> 
+  <li>Agarrar el jabon</li> 
+  <li>Aplicar en toda el area del cuerpo</li> 
+  <li>Enjuagarse </li> 
+  <li>Cerrar la canilla</li>
+  <li>Secarse con la toalla</li>
+  <li>Vestirse con ropa nueva</li>
+  <li>Agarrar la ropa sucia</li>
+  <li>Salir del baño</li>
+</ol>
+
+<img src="https://www.cadenadial.com/wp-content/uploads/2017/05/ducha-1.jpg" alt="ducha">
+`
